6279fb709d3519a5439a6e4860d9a030a91aeabc
oof2win2 <honza.koco44@gmail.com>
2021-11-26 08:55:14 +0100

    :sparles: functioning OTP code generation

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use std::env;
-use std::str;
+use base32;
 
 // use ootp::constants::*;
-use ootp::totp::{CreateOption, Totp};
-use data_encoding::BASE32;
+use totp_rs::{Algorithm, TOTP};
+use std::time::SystemTime;
 
 pub mod models;
 pub mod schema;
@@ -29,33 +29,27 @@ pub fn establish_connection() -> SqliteConnection {
 /**
  * Generate a TOTP string from a base32 encoded secret
  */
-fn generate_totp(secret: &str) {
-	let totp = Totp::secret(secret, CreateOption::Default);
-	let otp = totp.make();
-	println!("OTP is {}", otp);
+fn generate_totp(secret: &Vec<u8>) -> String {
+	let totp = TOTP::new(
+		Algorithm::SHA1,
+		6,
+		1,
+		30,
+		secret,
+	);
+	let time = SystemTime::now()
+		.duration_since(SystemTime::UNIX_EPOCH).unwrap()
+		.as_secs();
+	let otp = totp.generate(time);
+	return otp
 }
 
 fn main() {
-	// let connection = establish_connection();
-	// let results = services
-	// 	.load::<Service>(&connection)
-	// 	.expect("Error loading services");
-	
-	// println!("Displaying {} posts", results.len());
-    // for service in results {
-    //     println!("ID {}: {}", service.id, service.secret);
-    // }
-	
-	let result = match BASE32.decode("IEQHG5DSN5XGOIDTNBQXEZLEEBZWKY3SMV2A====".as_bytes()) {
-		Ok(data) => {
-			data
-		}
-		Err(error) => {
-			println!("{}", error);
-			[0].to_vec()
-		}
-	};
-	let strres = str::from_utf8(&result).unwrap();
-	println!("{}", strres);
-	generate_totp(&strres);
+	let input = "DG4ERUOUMURJHI77I4HZW53WUFMRNUNG";
+	let result = base32::decode(base32::Alphabet::RFC4648{padding: true}, input).unwrap();
+	for i in 1..result.len() {
+		print!("{:#x} ", result[i]);
+	}
+	println!("");
+	println!("{}", generate_totp(&result));
 }
